61fecc5c512ddbcdba32ba1ba2f3744386b7949d
Nishant7620 <nkaranjavkar72@gmail.com>
2023-12-26 21:07:33 +0530

    passing parameter from function

---
+++ passing parameter from function.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    result = a+b
+    print(result)
+
+add(8,2)
+
+#--------------------------------------------------------------------------------
+
+def calculate(a,b):
+    add = a+b
+    sub = a-b
+    print("addition:",add)
+    print("substraction:",sub)
+
+calculate(5,3)    
