4a1317e8cc4d1cb6181390943316c1513b4209cf
Nishant7620 <nkaranjavkar72@gmail.com>
2023-12-26 21:11:14 +0530

    updated

--- passing parameter from function.py
+++ passing parameter from function.py
@@ -12,4 +12,24 @@ def calculate(a,b):
     print("addition:",add)
     print("substraction:",sub)
 
-calculate(5,3)    
+calculate(5,3) 
+
+#--------------------------------------------------------------------------------
+
+def add(a,b):
+    result = a+b
+    print(result)
+
+def sub(a,b):
+    result = a-b
+    print(result)
+
+def mul(a,b):
+    result = a*b
+    print(result)
+
+def calculation():
+    add(5,3)
+    sub(5,3)
+    mul(5,3)
+calculation()    
