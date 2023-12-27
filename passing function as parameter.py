6a20a9b7f33a6d72c2bdf3d6a3e33ff01bd2b32d
Nishant7620 <nkaranjavkar72@gmail.com>
2023-12-27 21:04:40 +0530

    updated

--- passing function as parameter.py
+++ passing function as parameter.py
@@ -7,3 +7,22 @@ def show():
 
 
 print(display(show()))
+
+#-----------------------------------------------------------------------------------
+
+def add(x,y):
+    return x+y
+
+def mul(x,y):
+    return x*y
+
+
+def operation(operate,x,y):
+    return operate(x,y)
+
+result = operation(add,5,3)
+
+print(result)
+
+result2 = operation(mul,5,3)
+print(result2)   
