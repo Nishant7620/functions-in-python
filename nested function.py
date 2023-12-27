10dc99c12a0b624ebfac55fd1652a6a99a6d29e7
Nishant7620 <nkaranjavkar72@gmail.com>
2023-12-27 20:59:30 +0530

    passing function as parameter

--- nested function.py
+++ nested function.py
@@ -16,3 +16,5 @@ def outer():
 x = outer()
 print(x)
 
+
+
---
+++ passing function as parameter.py
@@ -0,0 +1,9 @@
+def display(sh):
+    return "this is display function " + sh
+
+
+def show():
+    return "this is show function"
+
+
+print(display(show()))
