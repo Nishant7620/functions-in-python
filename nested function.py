2278301d44bf762dceb58735505af65d0d16db99
Nishant7620 <nkaranjavkar72@gmail.com>
2023-12-27 20:54:52 +0530

    nested function with return statement

--- nested function.py
+++ nested function.py
@@ -6,4 +6,13 @@ def outer():
 outer()  
 
 #----------------------------------------------------------------------------------
+# nested function with return statement
+
+def outer():
+    def inner():
+        return "inner function"
+    return "outer function" " " + inner() 
+
+x = outer()
+print(x)
 
