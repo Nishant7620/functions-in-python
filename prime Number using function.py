7809b0b546692506b5e8870bc27ed493abb0cb8f
Nishant7620 <nkaranjavkar72@gmail.com>
2023-12-29 20:22:38 +0530

    updated

--- prime Number using function.py
+++ prime Number using function.py
@@ -2,3 +2,18 @@ num = int(input("Enter a number: "))
 def prime(num):
     if num<=1:
         return True
+
+    else:
+        for i in range(2,num):
+            if num%i==0:
+                return False
+        return True
+
+
+if prime(num):
+    print("prime")
+
+else:
+    print("not prime")
+prime(num)
+
