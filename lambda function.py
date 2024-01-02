1357318d9f54dffc3798ac3ef1f84b1ca68af150
Nishant7620 <nkaranjavkar72@gmail.com>
2024-01-02 10:32:05 +0530

    storing result in variable

--- lambda function.py
+++ lambda function.py
@@ -16,3 +16,14 @@ calculation = lambda a,b:(a+b,a-b,a*b)
 
 result = calculation(5,3)
 print(result)
+
+#----------------------------------------------------------------------------
+#storing result in varriable
+
+calc  = lambda a,b:(a+b,a*b,a-b)
+
+x,y,z = calc(5,3)
+
+print(x)
+print(y)
+print(z)
