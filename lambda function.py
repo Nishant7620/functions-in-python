66a14fec76617d56fc39caff330f6ca149be4732
Nishant7620 <nkaranjavkar72@gmail.com>
2024-01-02 10:29:19 +0530

    lambda function

---
+++ lambda function.py
@@ -0,0 +1,18 @@
+
+x =lambda  name : print(name)
+
+x("Nishant")
+
+#---------------------------------------------------------------------------
+
+add = lambda a,b : a+b
+
+result = add(5,3)
+print(result)
+
+#---------------------------------------------------------------------------
+
+calculation = lambda a,b:(a+b,a-b,a*b)
+
+result = calculation(5,3)
+print(result)
