330416c0840623f50743279218bdc9a7a111296b
Nishant7620 <nkaranjavkar72@gmail.com>
2024-01-05 20:14:16 +0530

    sum of digits of positive integer

--- recursive function.py
+++ recursive function.py
@@ -1,3 +1,4 @@
+"""
 def factorial(num):
     if num==0 or num ==1:
         return 1
@@ -8,7 +9,7 @@ def factorial(num):
 
 num = 5
 print(f" factorical of a number {num} is: {factorial(num)}")
-
+"""
 #--------------------------------------------------------------------------------
 
 def fibonacci(n):
@@ -19,3 +20,16 @@ def fibonacci(n):
         return  fibonacci(n-1)+ fibonacci(n-2)    
 n = 5
 print(f"Fibonacci series of {n} is: {fibonacci(5)}")
+
+#--------------------------------------------------------------------------------
+
+def sum_of_num(num):
+    if num<10:                      
+        return num
+
+    else:
+        return num% 10 + sum_of_num(num//10)
+
+num = 161
+
+print(f"sum of digits of {num} is:{sum_of_num(num)}")
